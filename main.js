cf658aa710cc834fe448e6488d58ae6f37e8bf24
DraXyPlayZ-037 <87331129+DraXyPlayZ-037@users.noreply.github.com>
2022-02-21 09:56:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var fr = ["Aarav Kunjir ", "Aayra Kunjir ", "Milind Kunjir ", "Sonali Kunjir ", ];
+
+var ima = ["download.png", "daughter.webp", "download(1).png", "mom-clipart.png"];
+
+var i = 0;
+
+function asd() {
+    document.getElementById("nameC").innerHTML = fr[i];
+
+    if (i > 0) {
+        if (i => 4) {
+            i = 0;
+        }
+
+    }
+
+
+};
+//second one just to insure they both hhave independent varibles(i,o)
+var k = 0
+
+function why() {
+    document.getElementById("picC").src = ima[i];
+
+    if (k > 0) {
+        if (k => 4) {
+            k = 0;
+        }
+
+    }
+
+
+};
